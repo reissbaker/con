f4af459e34c9d067b92a633559e65fcd518befdd
Matt Baker <matthew.reiss.baker@gmail.com>
2016-03-16 00:36:56 -0700

    Add more arithmetic fns to prelude

--- build/lib/interpreter/built-ins.js
+++ build/lib/interpreter/built-ins.js
@@ -4,13 +4,24 @@ var types_1 = require('./types');
 var primitive_1 = require('./primitive');
 var types = require('./types');
 function bindBuiltIns(scope) {
-    set(scope, {
-        name: '+',
+    set(scope, arithmetic('+', function (a, b) { return a + b; }));
+    set(scope, arithmetic('-', function (a, b) { return a - b; }));
+    set(scope, arithmetic('*', function (a, b) { return a * b; }));
+    set(scope, arithmetic('/', function (a, b) { return a / b; }));
+}
+exports.bindBuiltIns = bindBuiltIns;
+function set(scope, builder) {
+    var fnType = new types_1.FnType(builder.argTypes, builder.returnType);
+    scope.set(builder.name, new fn_1.Fn(fnType, builder.body));
+}
+function arithmetic(name, fn) {
+    return {
+        name: name,
         argTypes: [],
         returnType: null,
         body: function (scope, args) {
             if (args.length !== 2)
-                throw new Error("TODO: remove me. + should be typechecked");
+                throw new Error("TODO: remove me. " + name + " should be typechecked");
             var a = args[0];
             var b = args[1];
             if (a instanceof primitive_1.Primitive && b instanceof primitive_1.Primitive) {
@@ -18,17 +29,13 @@ function bindBuiltIns(scope) {
                 var bVal = b.value;
                 if (typeof aVal === 'number' && typeof bVal === 'number') {
                     // TODO: handle floats
-                    return new primitive_1.Primitive(types.PrimitiveType.Int32, aVal + bVal);
+                    var val = fn(a.value, b.value);
+                    return new primitive_1.Primitive(types.PrimitiveType.Int32, val);
                 }
-                throw new Error("TODO: remove me. + should be typechecked");
+                throw new Error("TODO: remove me. " + name + " should be typechecked");
             }
-            throw new Error("TODO: remove me. + should be typechecked");
+            throw new Error("TODO: remove me. " + name + " should be typechecked");
             return null;
-        },
-    });
-}
-exports.bindBuiltIns = bindBuiltIns;
-function set(scope, builder) {
-    var fnType = new types_1.FnType(builder.argTypes, builder.returnType);
-    scope.set(builder.name, new fn_1.Fn(fnType, builder.body));
+        }
+    };
 }
--- lib/interpreter/built-ins.ts
+++ lib/interpreter/built-ins.ts
@@ -9,29 +9,10 @@ import { Primitive } from './primitive';
 import * as types from './types';
 
 export function bindBuiltIns(scope: ScopeTree) {
-  set(scope, {
-    name: '+',
-    argTypes: [],
-    returnType: null,
-    body(scope: ScopeTree, args: Value[]): Value {
-      if(args.length !== 2) throw new Error(`TODO: remove me. + should be typechecked`);
-      const a = args[0];
-      const b = args[1];
-      if(a instanceof Primitive && b instanceof Primitive) {
-        const aVal = a.value;
-        const bVal = b.value;
-
-        if(typeof aVal === 'number' && typeof bVal === 'number') {
-          // TODO: handle floats
-          return new Primitive(types.PrimitiveType.Int32, aVal + bVal);
-        }
-        throw new Error(`TODO: remove me. + should be typechecked`);
-      }
-      throw new Error(`TODO: remove me. + should be typechecked`);
-      return null;
-    },
-  });
-
+  set(scope, arithmetic('+', (a, b) => a + b));
+  set(scope, arithmetic('-', (a, b) => a - b));
+  set(scope, arithmetic('*', (a, b) => a * b));
+  set(scope, arithmetic('/', (a, b) => a / b));
 }
 
 interface FnBuilder {
@@ -44,4 +25,33 @@ interface FnBuilder {
 function set(scope: ScopeTree, builder: FnBuilder) {
   const fnType = new FnType(builder.argTypes, builder.returnType);
   scope.set(builder.name, new Fn(fnType, builder.body));
+}
+
+function arithmetic(name: string, fn: (a: number, b: number) => number): FnBuilder {
+  return {
+    name,
+    argTypes: [],
+    returnType: null,
+    body(scope: ScopeTree, args: Value[]) {
+      if(args.length !== 2) throw new Error(`TODO: remove me. ${name} should be typechecked`);
+      const a = args[0];
+      const b = args[1];
+
+      if(a instanceof Primitive && b instanceof Primitive) {
+        const aVal = a.value;
+        const bVal = b.value;
+
+        if(typeof aVal === 'number' && typeof bVal === 'number') {
+          // TODO: handle floats
+          const val = fn(a.value, b.value);
+          return new Primitive(types.PrimitiveType.Int32, val);
+        }
+
+        throw new Error(`TODO: remove me. ${name} should be typechecked`);
+      }
+
+      throw new Error(`TODO: remove me. ${name} should be typechecked`);
+      return null;
+    }
+  };
 }
